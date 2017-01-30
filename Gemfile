368d452a67a6b0d2fc9450602d3ba0b0c970cd0b
Chris Zimmerman <russian478@gmail.com>
2017-01-30 09:51:15 -0700

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+
+# debugger gym
+# use binding.pry to enter debugger
+gem 'pry', '~> 0.14.0'
+
+# Color terminal output
+gem 'colorize', '~> 0.8.1'
---
+++ casino.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require 'colorize'
+require_relative 'player'
+
+class Casino
+  # Todo: handle multiple players
+  # Hint: Think array
+  attr_accessor :player
+
+  def initialize # .new always goes back to initialize
+    puts 'Welcome To The Casino'.colorize(:blue)
+    @player = Player.new
+    puts "What game do you want to play #{player.name}?"
+    menu
+    # show a casino game menu
+    # let the player choose a game
+    # initialize the new game passing the player as a parameter
+  end
+
+  def menu
+  end
+end
+
+Casino.new
